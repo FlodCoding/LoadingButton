b51be1d7921701e3ef8285c78e642cedb8d7c7ec
Flod <openthedoorcia@163.com>
2019-06-24 18:55:19 +0800

    add DrawableTextView

--- Loadingbutton/src/main/java/com/flod/loadingbutton/DrawableTextView.java
+++ Loadingbutton/src/main/java/com/flod/loadingbutton/DrawableTextView.java
@@ -1,13 +1,15 @@
 package com.flod.loadingbutton;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.ViewGroup;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatTextView;
 
@@ -16,69 +18,151 @@ import androidx.appcompat.widget.AppCompatTextView;
  * Creator: Flood
  * Date: 2019-06-13
  * UseDes:
- * TODO 设置Drawable的大小
+ * 1、Gravity() = center 文字居中但图片没有居中 √
+ * 2、设置Drawable的大小 √
  */
 public class DrawableTextView extends AppCompatTextView {
+
+    @IntDef({INDEX.START, INDEX.TOP, INDEX.END, INDEX.BOTTOM})
+    @interface INDEX {
+        int START = 0;
+        int TOP = 1;
+        int END = 2;
+        int BOTTOM = 3;
+
+        int W = 0;
+        int H = 1;
+    }
+
+
     protected Drawable[] mDrawables;
+    private float[][] mDrawableSize = new float[4][2];
+
     private float mTextWidth;
     private float mTextHeight;
+    private Rect mTextBounds = new Rect();
 
     private boolean isCenter;  //是否是居中
+    private boolean enableDrawablesCenter;
 
     public DrawableTextView(Context context) {
-        this(context, null);
+        super(context);
+        init(context, null);
     }
 
     public DrawableTextView(Context context, AttributeSet attrs) {
-        this(context, attrs, android.R.attr.textViewStyle);
+        super(context, attrs);
+        init(context, attrs);
     }
 
     public DrawableTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        //测量文字的长度
-        mTextWidth = measureTextWidth(getText().toString());
-        mDrawables = getCompoundDrawablesRelative();
+        init(context, attrs);
+
     }
+
+    private void init(Context context, AttributeSet attrs) {
+        mDrawables = getCompoundDrawablesRelative();
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.DrawableTextView);
+        enableDrawablesCenter = array.getBoolean(R.styleable.DrawableTextView_enableDrawableCenter, true);
+        if (mDrawables[INDEX.START] != null) {
+            mDrawableSize[INDEX.START] = new float[]{
+                    array.getDimension(R.styleable.DrawableTextView_drawableStartWidth, mDrawables[INDEX.START].getIntrinsicWidth()),
+                    array.getDimension(R.styleable.DrawableTextView_drawableStartWidth, mDrawables[INDEX.START].getIntrinsicHeight())};
+        }
+
+        if (mDrawables[INDEX.TOP] != null) {
+            mDrawableSize[INDEX.TOP] = new float[]{
+                    array.getDimension(R.styleable.DrawableTextView_drawableTopWidth, mDrawables[INDEX.TOP].getIntrinsicWidth()),
+                    array.getDimension(R.styleable.DrawableTextView_drawableTopHeight, mDrawables[INDEX.TOP].getIntrinsicHeight())};
+        }
+
+        if (mDrawables[INDEX.END] != null) {
+            mDrawableSize[INDEX.END] = new float[]{
+                    array.getDimension(R.styleable.DrawableTextView_drawableRightWidth, mDrawables[INDEX.END].getIntrinsicWidth()),
+                    array.getDimension(R.styleable.DrawableTextView_drawableRightHeight, mDrawables[INDEX.END].getIntrinsicHeight())};
+        }
+
+        if (mDrawables[INDEX.BOTTOM] != null) {
+            mDrawableSize[INDEX.BOTTOM] = new float[]{
+                    array.getDimension(R.styleable.DrawableTextView_drawableBottomWidth, mDrawables[INDEX.BOTTOM].getIntrinsicWidth()),
+                    array.getDimension(R.styleable.DrawableTextView_drawableBottomHeight, mDrawables[INDEX.BOTTOM].getIntrinsicHeight())};
+        }
+
+        array.recycle();
+
+        //resetCompoundDrawables
+        resetDrawablesSize();
+    }
+
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        //宽度并不是固定的,且是居中
-        //TODO 不只有center
-        if (getLayoutParams().width != ViewGroup.LayoutParams.WRAP_CONTENT && getGravity() == Gravity.CENTER) {
-            isCenter = true;
-        } else {
-            isCenter = false;
+        if (enableDrawablesCenter) {
+            isCenter = getLayoutParams().width != ViewGroup.LayoutParams.WRAP_CONTENT
+                    && ((getGravity() & Gravity.CENTER) == Gravity.CENTER
+                    || (getGravity() & Gravity.CENTER_HORIZONTAL) == Gravity.CENTER_HORIZONTAL
+                    || (getGravity() & Gravity.CENTER_VERTICAL) == Gravity.CENTER_VERTICAL);
+
+
+            mTextWidth = measureTextWidth();
+            mTextHeight = measureTextHeight();
         }
         super.onLayout(changed, left, top, right, bottom);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-
-        //重新绘制
-        if (isCenter) {
-            //Horizontal adjust
-            if (mDrawables[0] != null) {
-                final int offset = (int) calcOffset(mDrawables[0], true);
-                mDrawables[0].getBounds().offset(offset, 0);
-
+        if (enableDrawablesCenter && isCenter) {
+            if (mDrawables[INDEX.START] != null) {
+                mDrawables[INDEX.START].getBounds().offset((int) calcOffset(INDEX.START), 0);
             }
 
-            if (mDrawables[2] != null) {
-                final int offset = (int) calcOffset(mDrawables[2], true);
-                mDrawables[2].getBounds().offset(-offset, 0);
+            if (mDrawables[INDEX.TOP] != null) {
+                mDrawables[INDEX.TOP].getBounds().offset(0, (int) calcOffset(INDEX.TOP));
             }
 
+            if (mDrawables[INDEX.END] != null) {
+                mDrawables[INDEX.END].getBounds().offset(-(int) calcOffset(INDEX.END), 0);
+            }
+            if (mDrawables[INDEX.BOTTOM] != null) {
+                mDrawables[INDEX.BOTTOM].getBounds().offset(0, -(int) calcOffset(INDEX.BOTTOM));
+            }
         }
         super.onDraw(canvas);
     }
 
-    private float calcOffset(Drawable drawable, boolean isHorizontal) {
-        if (isHorizontal) {
-            return (getWidth() - (drawable.getIntrinsicWidth() + getCompoundDrawablePadding() + mTextWidth)) / 2;
-        } else {
-            return (getHeight() - (drawable.getIntrinsicHeight() + getCompoundDrawablePadding() + mTextHeight)) / 2;
+    private float calcOffset(@INDEX int position) {
+        int extraOffset = 0;
+        switch (position) {
+            case INDEX.START:
+                if (mDrawables[INDEX.END] != null) {
+                    extraOffset = getCompoundPaddingEnd();
+                }
+                return (getWidth() - (mDrawableSize[INDEX.START][INDEX.W] + mTextWidth + extraOffset)) / 2;
+
+            case INDEX.TOP:
+                if (mDrawables[INDEX.BOTTOM] != null) {
+                    extraOffset = getCompoundPaddingBottom();
+                }
+                return (getHeight() - (mDrawableSize[INDEX.TOP][INDEX.H] + mTextHeight + extraOffset)) / 2;
+
+            case INDEX.END:
+                if (mDrawables[INDEX.START] != null) {
+                    extraOffset = getCompoundPaddingStart();
+                }
+                return (getWidth() - (mDrawableSize[INDEX.END][INDEX.W] + mTextWidth + extraOffset)) / 2;
+
+            case INDEX.BOTTOM:
+                if (mDrawables[INDEX.TOP] != null) {
+                    extraOffset = getCompoundPaddingTop();
+                }
+                return (getHeight() - (mDrawableSize[INDEX.BOTTOM][INDEX.H] + mTextHeight + extraOffset)) / 2;
+            default:
+                return 0;
+
         }
     }
 
@@ -105,23 +189,29 @@ public class DrawableTextView extends AppCompatTextView {
         }
     }
 
-    @Override
-    public void setText(CharSequence text, BufferType type) {
-        super.setText(text, type);
-        //测量文字的长度
-        mTextWidth = measureTextWidth(text.toString());
-        mTextHeight = measureTextHeight(text.toString());
+    private void resetDrawablesSize() {
+        for (int i = 0; i < mDrawables.length; i++) {
+            if (mDrawables[i] != null) {
+                mDrawables[i].getBounds().right =
+                        (int) (mDrawables[i].getBounds().left + mDrawableSize[i][INDEX.W]);
+                mDrawables[i].getBounds().bottom =
+                        (int) (mDrawables[i].getBounds().top + mDrawableSize[i][INDEX.H]);
+            }
+        }
+        setCompoundDrawablesRelative(mDrawables[0], mDrawables[1], mDrawables[2], mDrawables[3]);
     }
 
-    private float measureTextWidth(String text) {
-        return text != null ? getPaint().measureText(text) : 0;
+
+    private float measureTextWidth() {
+        getLineBounds(0, mTextBounds);
+        float width = getPaint().measureText(getText().toString());
+        float maxWidth = mTextBounds.right - mTextBounds.left;
+        return width <= maxWidth ? width : maxWidth;
     }
 
-    private float measureTextHeight(String text) {
-        if (text == null)
-            return 0;
-        Paint.FontMetrics fontMetrics = getPaint().getFontMetrics();
-        return (fontMetrics.descent - fontMetrics.ascent) / 2;
+    private float measureTextHeight() {
+        return getLineHeight() * getLineCount();
     }
+
 
 }
